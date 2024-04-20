f365bf4c8aeaae06482a0eab3c01bc144b9b00d6
raviGitHub <84890981+655029@users.noreply.github.com>
2024-04-20 17:04:47 +0530

    add textfeild and border

---
+++ FloatingTextFeildiOS/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FloatingTextFeildiOS
+//
+//  Created by Ravi on 20/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func eyeAction(_ sender: UIButton) {
+        sender.isSelected.toggle()
+    }
+
+}
+
